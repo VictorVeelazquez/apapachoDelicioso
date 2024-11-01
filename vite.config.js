1783a56c97612c9f8be5fd5f6e1a7a3efc8c34a3
Victor Velazquez <vicdansan@gmail.com>
2024-11-01 10:54:05 -0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/apapachoDelicioso/',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.vue'],
   },
